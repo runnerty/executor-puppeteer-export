45f3f770bcb35ee73bb7bc4e5a051f7729f8a46b
Carlos Carmona Alcantara <carlos@iMac.local>
2020-05-09 11:57:29 +0200

    build: 2.0.0

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
+<p align="center">
+  <a href="http://runnerty.io">
+    <img height="257" src="https://runnerty.io/assets/header/logo-stroked.png">
+  </a>
+  <p align="center">A new way for processes managing</p>
+</p>
+
+[![NPM version][npm-image]][npm-url] [![Downloads][downloads-image]][npm-url] [![Dependency Status][david-badge]][david-badge-url] 
+<a href="#badge">
+  <img alt="code style: prettier" src="https://img.shields.io/badge/code_style-prettier-ff69b4.svg">
+</a>
+
 # Executor for [Runnerty]: Puppeteer wrapper to export html to PDF/image 
 
 #### EJS Compatible
@@ -81,3 +93,8 @@
 
 
 [Runnerty]: http://www.runnerty.io
+[downloads-image]: https://img.shields.io/npm/dm/@runnerty/executor-shell.svg
+[npm-url]: https://www.npmjs.com/package/@runnerty/executor-shell
+[npm-image]: https://img.shields.io/npm/v/@runnerty/executor-shell.svg
+[david-badge]: https://david-dm.org/runnerty/executor-shell.svg
+[david-badge-url]: https://david-dm.org/runnerty/executor-shell
--- index.js
+++ index.js
@@ -5,14 +5,14 @@ const URL = require('url').URL;
 const ejs = require('ejs');
 const fs = require('fs');
 
-const stringIsAValidUrl = s => {
+function stringIsAValidUrl(url) {
   try {
-    new URL(s);
+    new URL(url);
     return true;
   } catch (err) {
     return false;
   }
-};
+}
 
 const Execution = global.ExecutionClass;
 
@@ -22,35 +22,26 @@ class puppeteerExportExecutor extends Execution {
   }
 
   async exec(opt) {
-    const _this = this;
-
     const browser = await puppeteer.launch(opt.launch || {});
     const page = await browser.newPage();
 
     if (opt.userAgent) page.setUserAgent(opt.userAgent);
 
-    if (opt.viewport && opt.viewport.width && opt.viewport.height)
-      await page.setViewport(opt.viewport);
+    if (opt.viewport && opt.viewport.width && opt.viewport.height) await page.setViewport(opt.viewport);
     if (opt.authenticate) await page.authenticate(opt.authenticate);
     if (opt.geolocation) await page.setGeolocation(opt.geolocation);
-    if (opt.extraHTTPHeaders)
-      await page.setExtraHTTPHeaders(opt.extraHTTPHeaders);
-    if (opt.bypassCSP)
-      await page.setBypassCSP(opt.bypassCSP === 'true' ? true : false);
-    if (opt.hasOwnProperty('offlineMode'))
-      await page.setOfflineMode(opt.offlineMode);
-    if (opt.hasOwnProperty('cacheEnabled'))
-      await page.setCacheEnabled(opt.cacheEnabled);
-    if (opt.hasOwnProperty('javaScriptEnabled'))
-      await page.setJavaScriptEnabled(opt.javaScriptEnabled);
+    if (opt.extraHTTPHeaders) await page.setExtraHTTPHeaders(opt.extraHTTPHeaders);
+    if (opt.bypassCSP) await page.setBypassCSP(opt.bypassCSP === 'true' ? true : false);
+    if (opt.hasOwnProperty('offlineMode')) await page.setOfflineMode(opt.offlineMode);
+    if (opt.hasOwnProperty('cacheEnabled')) await page.setCacheEnabled(opt.cacheEnabled);
+    if (opt.hasOwnProperty('javaScriptEnabled')) await page.setJavaScriptEnabled(opt.javaScriptEnabled);
 
     if (opt.html) {
       if (stringIsAValidUrl(opt.html) && !opt.html.startsWith('file://')) {
         await page.goto(opt.html, opt.options || {});
         if (opt.cookie) await page.setCookie(opt.cookie);
       } else {
-        if (opt.html.startsWith('file://'))
-          opt.html = opt.html.replace('file://', '');
+        if (opt.html.startsWith('file://')) opt.html = opt.html.replace('file://', '');
         let content = fs.readFileSync(opt.html, 'utf8');
         if (opt.args) {
           console.log(opt.args, Object.keys(opt.args).length);
@@ -65,7 +56,7 @@ class puppeteerExportExecutor extends Execution {
               err_output: `Error Puppeteer Export: ${err}`,
               msg_output: ''
             };
-            _this.end(endOptions);
+            this.end(endOptions);
           }
         }
         await page.setContent(content, opt.options || {});
@@ -74,7 +65,7 @@ class puppeteerExportExecutor extends Execution {
       if (opt.pdf) await page.pdf(opt.pdf);
       if (opt.screenshot) await page.screenshot(opt.screenshot);
     }
-    _this.end();
+    this.end();
     await browser.close();
   }
 }
